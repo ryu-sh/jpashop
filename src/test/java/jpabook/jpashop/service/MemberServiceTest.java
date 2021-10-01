1201718ebf9f9f5d67f1f6a0f91b41a0d6b530c1
shryu <>
2021-10-01 19:49:53 +0900

    feat : member 도메인 개발

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        Member member = new Member();
+        member.setName("kim");
+
+        Long savedId = memberService.join(member);
+
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    void 중복_회원_예외() {
+        Member member = new Member();
+        member.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        memberService.join(member);
+
+        Assertions.assertThatThrownBy(() ->
+                memberService.join(member2)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
